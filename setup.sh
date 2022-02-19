d9cbdb7220b35db0cf89432546f88f23699d1dad
Hiroshi Ochiai <thumbandpull@gmail.com>
2022-02-19 18:49:19 +0900

    feat: Add environment variables for git user.name, user.email

--- setup.sh
+++ setup.sh
@@ -36,14 +36,26 @@ for file in $(find . -type f); do
   esac
 done
 
+post_script=
 if ! git config user.name 1>/dev/null; then
-  read -p "Input git user.name: " git_user_name
-  git config --global user.name "${git_user_name}"
+  if [ -n "${GIT_USER_NAME}" ]; then
+    git config --global user.name ${GIT_USER_NAME}
+  else
+    post_script+="\n  git config --global user.name \${GIT_USER_NAME}"
+  fi
 fi
-
 if ! git config user.email 1>/dev/null; then
-  read -p "Input git user.email: " git_user_email
-  git config --global user.email "${git_user_email}"
+  if [ -n "${GIT_USER_EMAIL}" ]; then
+    git config --global user.email ${GIT_USER_EMAIL}
+  else
+    post_script+="\n  git config --global user.email \${GIT_USER_EMAIL}"
+  fi
 fi
 
 echo "Done"
+
+if [ -n "${post_script}" ]; then
+  echo    ""
+  echo -n "You might want to run following scripts:"
+  echo -e "${post_script}"
+fi
